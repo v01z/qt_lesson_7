25e29b4bba3912bdae91301c20206d6772ef6e51
knight <cyberhoffman@gmail.com>
2022-02-14 10:35:20 +0300

    Ready

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include <QMessageBox>
-#include <QDebug>
 #include <QtMath>
 
 
@@ -22,98 +21,75 @@ MainWindow::MainWindow(QWidget*  parent )
 
 MainWindow::~MainWindow()
 {
-
-    qDebug () << "***********Deleting vector elements ***";
-    qDebug() << "Vec size is: " << itemsVec.size();
-
-    for (auto &item : itemsVec)
-    {
-
-        qDebug () << item;
-
-        itemsVec.removeOne(item);
-
-        qDebug() << "Vec after deleting element is: " << itemsVec.size();
-        qDebug() << "Vec's count is: " << itemsVec.count();
-    }
-
-    //иногда вектор весь не очищается. вернёмся сюда позже
-    qDebug() << "Vec size after deleting all elements is: " << itemsVec.size();
-
-    for (const auto &elem : itemsVec)
-        qDebug () << elem;
-
-    qDebug () << "***********Deleting scene's elements ***";
-    qDebug() << "Scene has items: " << scene->items().count();
-
     for (auto &item : scene->items())
     {
-
-        qDebug() << "Removing from scene item: " << item;
+        //Вот это делать было необязательно:
         scene->removeItem(item);
 
+        //Цикл обхода сцены затевался только
+        //ради такого действа:
         delete item;
     }
-
-    qDebug() << "Scene has items now: " << scene->items().count();
-
-    //Лишний код
-    if (scene)
-        delete scene;
 }
 
 void MainWindow::mousePressEvent(QMouseEvent *mouseEvent)
 {
 
 
-    //------------ При нажатии правой кнопки на итеме удаляем его. Begin:---------
+    //------------ При нажатии правой кнопки на итеме удаляем его ---------------
+    //----------------------------- Begin: --------------------------------------
 
     if (mouseEvent->button() == Qt::RightButton)
     {
-//        if (!onEmptyPlaceClicked(mouseEvent) && items(mouseEvent->pos()).size() > 0)
         if (!onEmptyPlaceClicked(mouseEvent))
         {
-            //Если не на пустом месте, то currentItem должен содержать valid-значение
+            //Если клик - не на пустом месте, то currentItem должен
+            //содержать не-NULL-значение
             assert (currentItem != nullptr);
-
-            qDebug() << currentItem;
 
             removeOneItem(currentItem);
             currentItem = nullptr;
-            assert(currentItem == nullptr); //debug only
         }
 
         return;
     }
 
-    //-----------End of нажaтии правой -------------------------------------------
+    //----------------------End of нажатии правой.-------------------------------
 
     if (mouseEvent->button() != Qt::LeftButton)
         return;
 
 
-    //----- При нажатии левой кнопки на пустом месте создаём новый итем. Begin:-
+    //----- При нажатии левой кнопки на пустом месте создаём новый итем --------
+    //----------------------------- Begin: -------------------------------------
 
     if (onEmptyPlaceClicked(mouseEvent))
     {
         assert(currentItem == nullptr);
-        assert(createNewItem(mapToScene(mouseEvent->pos())) != nullptr);
+
+        QGraphicsItem *veryShortLifePointerThatOnlyNeedsForAssertBelow
+            { createNewItem(mapToScene(mouseEvent->pos())) };
+
+        assert(veryShortLifePointerThatOnlyNeedsForAssertBelow != nullptr);
 
         return;
     }
 
-    //-----------End of нажатие левой  ------------------------------------------
+    //--------------End of нажатие левой на пустом месте.------------------------
 
-    //----- При зажимании левой кнопки на итеме захватываем его. Begin:-
+    //----- При зажимании левой кнопки на итеме захватываем его -----------------
+    //------------------------------ Begin: -------------------------------------
 
     else // !onEmptyPlaceclicked
     {
         this->setCursor(QCursor(Qt::ClosedHandCursor));
+
         currentItem = scene->itemAt(mapToScene(mouseEvent->pos()), QTransform());
+
         willBeDragged = true;
     }
 
-    //-----------End of нажатие левой  ------------------------------------------
+    //-------------------- End of нажатие левой на итеме. ------------------------
 }
 
 void MainWindow::mouseReleaseEvent(QMouseEvent *mouseEvent)
@@ -122,7 +98,9 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *mouseEvent)
     if (!willBeDragged)
         return;
 
-    //----- При отпускании левой кнопки переносим итем на место курсора-------
+    //----- При отпускании левой кнопки переносим итем на место курсора ----------
+    //------------------------------ Begin: --------------------------------------
+
     if (mouseEvent->button() == Qt::LeftButton)
     {
 
@@ -133,12 +111,12 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *mouseEvent)
         currentItem = nullptr;
     }
 
+    //-------------- End of отпускание левой кнопки мыши. -------------------------
+
     else //Релизнута какая-то другая кнопка
             return;
-
 }
 
-//I will use this func to rotate items, если будет свободное время
 void MainWindow::mouseMoveEvent(QMouseEvent *mouseEvent)
 {
     if (!willBeDragged)
@@ -149,10 +127,12 @@ void MainWindow::mouseMoveEvent(QMouseEvent *mouseEvent)
     currentItem->setPos(mapToScene(mouseEvent->pos()));
 }
 
-//Если false, то будет currentItem будет valid, иначе currentItem будет nullptr
+//Если false, то currentItem будет содержать адрес на
+//QGraphicsItem в куче, иначе - nullptr
 bool MainWindow::onEmptyPlaceClicked(QMouseEvent *mouseEvent)
 {
-    //Получение currentItem есть побочный продукт данной функции
+    //Присвоение адреса на итем переменной-указателю currentItem
+    //есть побочный продукт работы данной функции
     currentItem = scene->itemAt(mapToScene(mouseEvent->pos()), QTransform());
 
     if (currentItem == nullptr)
@@ -167,32 +147,39 @@ QGraphicsItem *MainWindow::createNewItem(const QPointF point)
     brush.setColor(QColor(rand() % 256, rand() % 256, rand() % 256));
     brush.setStyle(Qt::BrushStyle::SolidPattern);
 
+    //"Красный" - значит "красивый" xD
     QPen pen(QColor("red"));
 
     QGraphicsItem *newItem;
 
     switch (figureIndex)
-    {
-    case 0:
-    {
-        QGraphicsRectItem *rectItem = new QGraphicsRectItem(point.x(), point.y(),
-                                70, 80);
+     {
+    case 0: //Прямоугольник
+     {
+        QGraphicsRectItem *rectItem =
+            new QGraphicsRectItem(point.x(), point.y(), 70, 80);
+
         rectItem->setPen(pen);
         rectItem->setBrush(brush);
+
         newItem = rectItem;
+
         break;
-    }
-    case 1:
-    {
-        QGraphicsEllipseItem *ellipseItem = new QGraphicsEllipseItem(point.x(), point.y(),
-                                70, 80);
+     }
+    case 1: //Эллипс
+     {
+        QGraphicsEllipseItem *ellipseItem =
+            new QGraphicsEllipseItem(point.x(), point.y(), 70, 80);
+
         ellipseItem->setPen(pen);
         ellipseItem->setBrush(brush);
+
         newItem = ellipseItem;
+
         break;
-    }
+     }
     case 2: //Пятиконечная звезда
-    {
+     {
         qreal X = point.x();
         qreal Y = point.y();
         qreal sz = 50; //размер звезды
@@ -216,29 +203,30 @@ QGraphicsItem *MainWindow::createNewItem(const QPointF point)
         polygon << p1 << p2 << p3 << p4 << p5 << p6 << p7 << p8 << p9 << p10;
 
         QGraphicsPolygonItem *starItem = new QGraphicsPolygonItem (polygon);
+
         starItem->setPen(pen);
         starItem->setBrush(brush);
+
         newItem = starItem;
+
         break;
-    }
+     }
     default:
-    {
-        qDebug() << "How did u managed get here?";
+     {
+    //    qDebug() << "How did u managed get here?";
         break;
-    }
+     }
+
     }
 
     itemsVec.push_back(newItem);
-
-    qDebug() << "Added new element: " << newItem;
-    qDebug() << "Vec size is now: " << itemsVec.size();
 
     scene->addItem(newItem);
 
     figureIndex++;
 
-    if (figureIndex > 2)
-     figureIndex = 0;
+    if (figureIndex > 2)   // У нас только три модели фигуры,
+          figureIndex = 0; // поэтому заходим на новый круг
 
     return newItem;
 }
@@ -250,21 +238,9 @@ void MainWindow::removeOneItem(QGraphicsItem *item)
 
     assert(itemsVec.size() == scene->items().count());
 
-    qDebug() << "+++++ Removing one element from vector +++++";
-    qDebug() << "Vector size before is: " << itemsVec.size();
-
     itemsVec.removeOne(item);
 
-    qDebug() << "Vector size after is: " << itemsVec.size();
-    qDebug () << '\n';
-
-    qDebug() << "***** Removing one element from scene*****";
-    qDebug() << "Scene itmes count before is: " << scene->items().count();
-
     scene->removeItem(item);
-
-    qDebug() << "Scene items count after isis: " << scene->items().count();
-    qDebug () << '\n';
 
     delete item;
 }
